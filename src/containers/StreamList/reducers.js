2440b0100686409aeff37b45e01f7ec2024de557
Victoria Komarovska <vkomarovska98@gmail.com>
2018-09-13 17:55:35 +0300

    minor modifications

--- src/containers/StreamList/reducers.js
+++ src/containers/StreamList/reducers.js
@@ -8,8 +8,18 @@ const initialState = {
 const StreamsReducer = (state = initialState, action) => {
     switch (action.type) {
       case FETCH_ALL:
-        console.log(action.payload);
-        return { ...state, Streams: action.payload, sortingType: 'all' };
+      const streams = action.payload.map(stream => {
+        const channelData = stream[0].data;
+        const streamData = stream[1].data;
+        return {
+          id: channelData.name,
+          name: channelData.display_name,
+          status: streamData.stream ? streamData.stream.game : 'offline',
+          userpic: channelData.logo,
+        };
+    }); 
+        console.log(streams);
+        return { ...state, Streams: [this.id, this.name, this.display_name, this.logo ], sortingType: 'all' };
 
       default:
         console.log(state);
